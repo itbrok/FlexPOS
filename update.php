9e8dc7acef94f3ec14ae1f9b83129871b05df227
brok <brokmtf@gmail.com>
2023-01-25 15:22:43 +0300

    Fix update.php errors

--- update.php
+++ update.php
@@ -4,8 +4,11 @@ define('DIR', dirname(__FILE__));
 
 $save = [basename(__FILE__), "update.zip"];
 
+ignore_user_abort(true);
+set_time_limit(3600);
+
 function deleteDir($dirPath) {
-    if (! is_dir($dirPath)) {
+    if (!is_dir($dirPath)) {
         die("$dirPath must be a directory");
     }
     if (substr($dirPath, strlen($dirPath) - 1, 1) != '/') {
@@ -16,10 +19,38 @@ function deleteDir($dirPath) {
         if (is_dir($file)) {
             deleteDir($file);
         } else {
-            if(!in_array(basename($file),$GLOBALS['save'])){
+            if (!in_array(basename($file), $GLOBALS['save'])) {
                 unlink($file);
             }
         }
+    }
+}
+function recurse_copy($src, $dst) {
+    $dir = opendir($src);
+    @mkdir($dst);
+    while (false !== ($file = readdir($dir))) {
+        if (($file != '.') && ($file != '..')) {
+            if (is_dir($src . '/' . $file)) {
+                recurse_copy($src . '/' . $file, $dst . '/' . $file);
+            } else {
+                copy($src . '/' . $file, $dst . '/' . $file);
+            }
+        }
+    }
+    closedir($dir);
+}
+
+function rrmdir($dir) {
+    if (is_dir($dir)) {
+        $objects = scandir($dir);
+        foreach ($objects as $object) {
+            if ($object != "." && $object != "..") {
+                if (filetype($dir . "/" . $object) == "dir") rrmdir($dir . "/" . $object);
+                else unlink($dir . "/" . $object);
+            }
+        }
+        reset($objects);
+        rmdir($dir);
     }
 }
 function checkUpdate($update = false) {
@@ -30,10 +61,10 @@ function checkUpdate($update = false) {
     $po = new ProgressObj();
 
     //in records present, set loading text and display bar
-    if($update){
+    if ($update) {
         $po->text = "جلب معلومات التحديث";
-        $po->DisplayMeter(); 
-    
+        $po->DisplayMeter();
+
         $count = 10;
         $po->Calculate($count);
         str_repeat(' ', 4480);
@@ -45,14 +76,13 @@ function checkUpdate($update = false) {
     $url = $flex['update_link'];
     // Get version number and update URL
     $values = json_decode(file_get_contents($url), true);
-    
-    if($values["version"] != $flex['version']) {
-        if($update == true){
+
+    if ($values["version"] != $flex['version']) {
+        if ($update == true) {
             // assuming file.zip is in the same directory as the executing script.
             $file = "update.zip";
 
             $po->text = "تنزيل ملفات التحديث";
-            // Download the zip file that contains the new version
             // Download the zip file that contains the new version
             $dfile = fopen($file, 'w');
             $ch = curl_init();
@@ -123,10 +153,15 @@ function checkUpdate($update = false) {
                     $zip->close();
                     $po->Animate();
 
+
+                    $dest = "FlexPOS-main";
+                    recurse_copy($dest, ".");
+                    rrmdir($dest);
+                    
                     //push the content out to the browser
                     ob_flush();
                     flush();
-                    
+
                     $config_file = file_get_contents("config.php");
                     $config_file = str_replace(["#HOST", "#USERNAME", "#PASSWORD", "#DBNAME"], [$config_data["host"], $config_data["user"], $config_data["pass"], $config_data["dbna"]], $config_file);
                     file_put_contents("config.php", $config_file);
@@ -155,17 +190,17 @@ function checkUpdate($update = false) {
                     $po->text = "حدث خطا اثناء استخراج البيانات";
                 }
             }
-        }else{
+        } else {
             return ["ok" => true, 3]; // "There is a new version"
         }
-    }else{
+    } else {
         return ["ok" => false, 4]; //"There isn't a new version"
     }
 }
-if(@$_POST["check"] == 1){
+if (@$_POST["check"] == 1) {
     echo json_encode(checkUpdate());
 }
 
-if(@$_GET["update"] == 1){
+if (@$_GET["update"] == 1) {
     checkUpdate(true);
 }
